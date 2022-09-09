1bcff4826b2bf968143660583c21434be96d5f05
xtophe38 <christophe.naud@gmail.com>
2022-09-09 16:03:17 +0200

    Files creation

---
+++ en.dict.combodo-powerbi-integration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 2022
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+));
