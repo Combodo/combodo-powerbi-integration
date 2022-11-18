a3784aebc76d576f427b19ab2211e1c178824055
xtophe38 <christophe.naud@gmail.com>
2022-11-18 15:56:16 +0100

    Changes for 1.0.1 release

--- module.combodo-powerbi-integration.php
+++ module.combodo-powerbi-integration.php
@@ -5,11 +5,11 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-powerbi-integration/1.0.0',
+	'combodo-powerbi-integration/1.0.1',
 	array(
 		// Identification
 		//
-		'label' => 'Combodo PowerBI reporting template',
+		'label' => 'Reporting for PowerBI - Helpdesk view',
 		'category' => 'business',
 
 		// Setup
